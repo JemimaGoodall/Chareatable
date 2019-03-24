6f272828b9110631f847734f694c56bd76f092b6
Jemima <jemima.goodall@gmail.com>
2019-03-24 01:49:34 -0400

    finalfinal

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-      <h5 class="my-0 mr-md-auto font-weight-normal"><router-link to="/">Vue Starter</router-link></h5>
+      <h5 class="my-0 mr-md-auto font-weight-normal"><router-link to="/">Chareatable</router-link></h5>
       <nav v-if="user" class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-dark"><router-link to="/">Home</router-link><span class="sr-only">(current)</span></a>
         <a class="p-2 text-dark"><router-link :to="{ path: createFundraiserLink()}">Create A Fundraiser</router-link></a>
